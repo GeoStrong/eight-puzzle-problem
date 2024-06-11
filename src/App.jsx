fddfeab7cf0b111c69f6a975b8bfcba99208888b
GeoStrong <dzobavageorgij@gmail.com>
2024-06-11 14:01:10 +0400

    Remove commented out code and unused state variables in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,10 @@
 import { useState } from 'react';
 import useLogic from '../algorithms/logic';
 
-// const areStatesEqual = (state1, state2) => state1.join() === state2.join();
-
-// const getPossibleMoves = (state) => {
-//   const moves = [];
-//   const size = 3; // 3x3 puzzle
-//   const emptyIndex = state.indexOf(0);
-//   const row = Math.floor(emptyIndex / size);
-//   const col = emptyIndex % size;
-
-//   if (row > 0) moves.push(swap(state, emptyIndex, emptyIndex - size)); // Move up
-//   if (row < size - 1) moves.push(swap(state, emptyIndex, emptyIndex + size)); // Move down
-//   if (col > 0) moves.push(swap(state, emptyIndex, emptyIndex - 1)); // Move left
-//   if (col < size - 1) moves.push(swap(state, emptyIndex, emptyIndex + 1)); // Move right
-
-//   return moves;
-// };
-
-// const swap = (state, i, j) => {
-//   const newState = [...state];
-//   [newState[i], newState[j]] = [newState[j], newState[i]];
-//   return newState;
-// };
-
-// BFS Algorithm
-// const bfs = (initialState, goalState) => {
-//   const queue = [[initialState]];
-//   const visited = new Set();
-//   visited.add(initialState.join());
-
-//   while (queue.length > 0) {
-//     const path = queue.shift();
-//     const currentState = path[path.length - 1];
-
-//     if (areStatesEqual(currentState, goalState)) {
-//       return path;
-//     }
-
-//     for (const nextState of getPossibleMoves(currentState)) {
-//       const nextStateStr = nextState.join();
-//       if (!visited.has(nextStateStr)) {
-//         visited.add(nextStateStr);
-//         queue.push([...path, nextState]);
-//       }
-//     }
-//   }
-
-//   return null; // No solution found
-// };
-
-// const isSolvable = (state) => {
-//   let inversions = 0;
-//   const flatState = state.filter((tile) => tile !== 0);
-//   for (let i = 0; i < flatState.length; i++) {
-//     for (let j = i + 1; j < flatState.length; j++) {
-//       if (flatState[i] > flatState[j]) inversions++;
-//     }
-//   }
-//   return inversions % 2 === 0;
-// };
-
-// // const initialState = [1, 2, 0, 3, 4, 5, 6, 7, 8];
-// const goalState = [1, 2, 3, 4, 5, 6, 7, 8, 0];
-
 const App = () => {
-  // const [initialState, setInitialState] = useState([1, 2, 3, 4, 5, 6, 7, 8, 0]);
-  // const [inputValues, setInputValues] = useState(initialState.join(','));
-  // const [solution, setSolution] = useState([]);
   const [activeAlgorithm, setActiveAlgorithm] = useState('bfs');
   const { inputValues, solution, handleChange, handleSolve } =
     useLogic(activeAlgorithm);
-
-  // const handleChange = (e) => {
-  //   setInputValues(e.target.value);
-  // };
-
-  // const handleSolve = () => {
-  //   const initialArray = inputValues.split(',').map(Number);
-  //   if (
-  //     initialArray.length !== 9 ||
-  //     new Set(initialArray).size !== 9 ||
-  //     initialArray.includes(NaN)
-  //   ) {
-  //     alert('Please enter a valid configuration with numbers 0-8.');
-  //     return;
-  //   }
-  //   if (!isSolvable(initialArray)) {
-  //     alert('The puzzle configuration is not solvable.');
-  //     return;
-  //   }
-  //   setInitialState(initialArray);
-  //   const result = bfs(initialArray, goalState);
-  //   if (result) {
-  //     setSolution(result);
-  //   } else {
-  //     alert('No solution found');
-  //   }
-  // };
 
   return (
     <div className="text-center mt-10">
